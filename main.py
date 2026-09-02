e59e3215823da3245fa2eaa643c9e810f90e1adc
lucasfestevam97 <lucasfestevam97@gmail.com>
2026-09-02 14:16:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 @app.get("/produto/{codigo}")
 def buscar_produto(codigo: str):
-    # Converte a coluna para string e remove espaços extras
-    codigo_limpo = str(codigo).strip()
+    codigo_busca = str(codigo).strip()
     
-    # Faz o mesmo com a coluna do Excel
-    df_produtos['Código de Barras'] = df_produtos['Código de Barras'].astype(str).str.replace('\.0$', '', regex=True).str.strip()
+    # Garante que a coluna do Excel vire texto limpo sem casas decimais (.0)
+    coluna_codigos = df_produtos['Código de Barras'].astype(str).str.replace(r'\.0$', '', regex=True).str.strip()
     
-    resultado = df_produtos[df_produtos['Código de Barras'] == codigo_limpo]
+    resultado = df_produtos[coluna_codigos == codigo_busca]
     
     if not resultado.empty:
         prod = resultado.iloc[0]
